7ff15c8e1c148343b6b2541bbffeb35fbb9e35e1
Jessica Temitope <67561059+temijess@users.noreply.github.com>
2022-06-13 20:37:02 +0100

    initial commit

---
+++ I4G0135201QD/blog/models.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+# Create your models here.
+class post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField
+    Published_date = models.DateTimeField
+
